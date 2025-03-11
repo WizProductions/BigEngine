794e55cf754e067fd915ed7ca9db0981664b9d03
Wiz <53436312+Wizurth@users.noreply.github.com>
2025-03-11 21:08:05 +0100

    dza

--- src/EngineDev/Scripts/EntityScript.cpp
+++
@@ -1,9 +0,0 @@
-
-#include "pch.h"
-#include "EntityScript.h"
-#include "Entities/Entity.h"
-
-EntityScript::EntityScript(Entity& attachedEntity) :
-	m_AttachedEntity(&attachedEntity) {
-	m_AttachedEntity->m_AttachedScript = this;
-};
